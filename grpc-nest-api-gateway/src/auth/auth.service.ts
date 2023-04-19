ee308f378f50e5948aa669d64ecd972a63231b02
sambathchea <cheasambath@youdingapp.com>
2023-04-19 15:11:27 +0700

    api gateway

--- grpc-nest-api-gateway/src/auth/auth.service.ts
+++ grpc-nest-api-gateway/src/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { AUTH_SERVICE_NAME, AuthServiceClient } from './auth.pb';
+import { ClientGrpc } from '@nestjs/microservices';
+import { ValidateResponse } from './auth.pb';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private svc: AuthServiceClient;
+
+  @Inject(AUTH_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.svc = this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
+
+  public validate(token: string): Promise<ValidateResponse> {
+    return firstValueFrom(this.svc.validate({ token }));
+  }
+}
